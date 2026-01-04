cd629b046fa384222a52b5679e6fa67fd57fec9a
budRich <robstenklippa@gmail.com>
2026-01-04 21:52:16 +0100

    updated get-upstream script

--- get-upstream.sh
+++ get-upstream.sh
@@ -2,15 +2,19 @@
 
 # for v in 0.993 0.994 0.995 0.996 0.997 ; do ./get-upstream.sh "$v" ; done
 
+upstream_branch=auto
 upstream_version=${1:-0.997}
 zip_file="OneStepBack-v${upstream_version}.zip"
 url=http://www.vide.memoire.free.fr/pages/onestepback/"$zip_file"
-git checkout upstream || exit 1
+git checkout "$upstream_branch" || exit 1
 wget "$url"
 
 [[ -d OneStepBack ]] && rm -rf OneStepBack/
+[[ -d theme ]] && rm -rf theme/
 unzip "$zip_file"
+mv OneStepBack theme
+mv -f theme/gtk-3.0/gtk.css src/OneStepBack.css
 
-git add OneStepBack
+git add .
 git commit -m "Pulled Upstream v${upstream_version}"
 git checkout -
